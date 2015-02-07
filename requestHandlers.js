033051005df48a41c6ad42ed83c71bd09fb3cb27
chengzh2008 <chengzh2008@gmail.com>
2015-02-06 21:16:03 -0800

    blocking style bad performance

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,14 @@
 var handlers = exports = module.exports = {}; //jshint ignore:line
 handlers.start = function () {
     console.log('Request handler.start was called');
+
+    function sleep(milliSeconds) {
+        var startTime = new Date().getTime();
+        while(new Date().getTime() < startTime + milliSeconds);
+    }
+
+    sleep(10000);
+
     return 'Hello Start';
 };
 
