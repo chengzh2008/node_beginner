91be09bcc3c3a07c6f694a09da85705bf40d2261
chengzh2008 <chengzh2008@gmail.com>
2015-02-06 21:54:06 -0800

    handling post request

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,28 @@
 var exec = require('child_process').exec,
+    querystring = require('querystring');
     handlers = exports = module.exports = {}; //jshint ignore:line
-handlers.start = function (response) {
+handlers.start = function (response, postData) {
     console.log('Request handler.start was called');
-    var content = "empty";
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea><br>' + '<input type="submit" value="Submit text" />' + '</form>' +
+        '</body>' +
+        '</html>';
 
-    exec("find /",
-        {tiemout: 10000, maxBuffer: 20000 * 1024},
-        function (err, stdout, stderr) {
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.write(stdout);
-            response.end();
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(body);
+    response.end();
 
-        });
+
 };
 
-handlers.upload = function (response) {
+handlers.upload = function (response, postData) {
     console.log('Requet handler.upload was called');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello Upload');
+    response.write("You have sent: " + querystring.parse(postData).text);
     response.end();
 };
--- server.js
+++ server.js
@@ -5,10 +5,20 @@ var http = require('http'),
 
 server.start = function (route, handle) {
     function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
+        var postData = "";
+            pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received');
 
-        route(handle, pathname, response);
+        request.setEncoding('utf8');
+        request.addListener('data', function(postDataChunk){
+            postData += postDataChunk;
+            console.log('Recerved POST data chunk "' + postDataChunk + '".');
+        });
+
+        request.addListener('end', function(){
+            route(handle, pathname, response, postData);
+        })
+
     }
 
     http.createServer(onRequest).listen(8888);
