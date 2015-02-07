eac4c498804dd128dc20e90a90a19193b9e77714
chengzh2008 <chengzh2008@gmail.com>
2015-02-06 22:14:39 -0800

    handle file upload

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 'user strict';
 var http = require('http'),
     url = require('url'),
+    sys = require('sys'),
+    formidable = require('formidable'),
     server = exports = module.exports = {};  // jshint ignore:line
 
 server.start = function (route, handle) {
-    function onRequest(request, response) {
-        var postData = "";
-            pathname = url.parse(request.url).pathname;
-        console.log('Request for ' + pathname + ' received');
+    function onRequest(req, res) {
+        if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
+            // parse a file upload
+            var form = new formidable.IncomingForm();
+            form.parse(req, function (err, fields, files) {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write('Received upload: \n\n');
+                res.end(sys.inspect({fields: fields, files: files}));
+            });
+            return;
+        }
 
-        request.setEncoding('utf8');
-        request.addListener('data', function(postDataChunk){
-            postData += postDataChunk;
-            console.log('Recerved POST data chunk "' + postDataChunk + '".');
-        });
-
-        request.addListener('end', function(){
-            route(handle, pathname, response, postData);
-        })
+        // show a file upload form
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(
+            '<form action="/upload" enctpe = "multipart/form-data" ' +
+            'method="post">' +
+            '<input type="text" name="title"><br>' +
+            '<input type="file" name="upload" multiple="multiple"><br>' +
+            '<input type="submit" value="Upload">' +
+            '<form>'
+        );
 
     }
 
